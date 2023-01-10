f531c3e88879f7102660bc4b89dcc66eb3aef0f0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-11 04:05:20 +0600

    create searchQueryContext

---
+++ src/components/SearchQueryContext.js
@@ -0,0 +1,22 @@
+const { createContext, useContext, useState } = require("react");
+
+const SearchQueryContext = createContext();
+const SearchQueryUpdateContext = createContext();
+
+export const useSearchQuery = () => useContext(SearchQueryContext);
+
+export const useSearchQueryUpdate = () => useContext(SearchQueryUpdateContext);
+
+export function SearchQueryProvider({ children }) {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (newQuery) => setQuery(newQuery);
+
+  return (
+    <SearchQueryContext.Provider value={query}>
+      <SearchQueryUpdateContext.Provider value={updateQuery}>
+        {children}
+      </SearchQueryUpdateContext.Provider>
+    </SearchQueryContext.Provider>
+  );
+}
