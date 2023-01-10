0cff92b610b48611ce9e10a63910eff2892a4176
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-11 04:25:20 +0600

    create photoModal component

---
+++ src/components/PhotoModal.js
@@ -0,0 +1,65 @@
+import CloseIcon from "@mui/icons-material/Close";
+import { Backdrop, Fade, Grow, Modal, Paper } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import Loader from "../helpers/Loader";
+import PhotoCard from "./PhotoCard";
+
+const PhotoModal = ({ photo, open, handleClose }) => {
+  return (
+    <Modal
+      style={{ overflowY: "auto" }}
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Grow in={open}>
+        {photo || false ? (
+          <Fade in={open}>
+            <Box sx={modalStyle}>
+              <Paper
+                sx={{
+                  borderRadius: 100,
+                  padding: "5px 5px 0 5px",
+                  position: "absolute",
+                  right: -15,
+                  top: -10,
+                }}
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </Paper>
+              <PhotoCard
+                photo={photo}
+                showMoreDetails={true}
+                onAfterTagClick={handleClose}
+              />
+            </Box>
+          </Fade>
+        ) : (
+          <Box sx={modalStyle}>
+            <Paper>
+              <Loader />
+            </Paper>
+          </Box>
+        )}
+      </Grow>
+    </Modal>
+  );
+};
+
+const modalStyle = {
+  position: "absolute",
+  top: "4%",
+  left: { xs: "3%", sm: "20%" },
+  width: { xs: "90%", sm: "60vw" },
+  bgcolor: "background.paper",
+  borderRadius: 4,
+  boxShadow: 24,
+};
+
+export default PhotoModal;
