c6e58e37a7abeb570ee14975e41dd803f89c0277
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-11 04:25:20 +0600

    create TagList component

---
+++ src/components/TagList.js
@@ -0,0 +1,39 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useSearchQueryUpdate } from "./SearchQueryContext";
+
+const TagList = ({ tags, onAfterTagClick }) => {
+  const updateSearchQuery = useSearchQueryUpdate();
+
+  const updateSearchQueryWithTag = (tag) => {
+    updateSearchQuery(tag);
+    onAfterTagClick();
+  };
+
+  return (
+    <Box sx={tagListContainer}>
+      {tags.map((tag) => (
+        <Button
+          key={tag.title}
+          size="small"
+          disableElevation
+          onClick={() => updateSearchQueryWithTag(tag.title)}
+        >
+          {tag.title}
+        </Button>
+      ))}
+    </Box>
+  );
+};
+
+// Styles
+const tagListContainer = {
+  pt: 1,
+  display: "grid",
+  gridAutoFlow: "dense",
+  gridTemplateColumns: "repeat(auto-fill, minmax(120px, 1fr))",
+  gap: 2,
+};
+
+export default TagList;
