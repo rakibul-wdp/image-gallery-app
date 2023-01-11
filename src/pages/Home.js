261453884248b7bae9a376a071b316904c43ca99
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-12 04:05:20 +0600

    create Home page functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import Hero from '../components/Hero';
-import Navbar from '../components/Navbar';
-import PhotoGrid from '../components/PhotoGrid';
-import PhotoModal from '../components/PhotoModal';
-import Loader from '../helpers/Loader';
+import { Container, Paper, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import PhotoGrid from "../components/PhotoGrid";
+import PhotoModal from "../components/PhotoModal";
+import { AppThemeProvider, useSearchQuery } from "../context/ThemeContext";
+import {
+  fetchPhotoById,
+  fetchPhotos,
+  fetchPhotosByQuery
+} from "../helpers/api";
+import Loader from "../helpers/Loader";
 
 const Home = () => {
+  const [photos, setPhotos] = useState([]);
+  const [modalData, setModalData] = useState();
+  const [showHeader, setShowHeader] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const searchQuery = useSearchQuery("");
+
+  useEffect(() => {
+    setPhotos([]);
+    const delayDebounce = setTimeout(
+      () =>
+        searchQuery.length > 0
+          ? onSearchQueryChanged(searchQuery)
+          : onPageLoad(),
+      500
+    );
+    return () => clearTimeout(delayDebounce);
+  }, [searchQuery]);
+
+  const handleModalClose = () => setModalOpen(false);
+
+  const onPageLoad = async () => {
+    const photos = await fetchPhotos();
+    setPhotos(photos);
+    setShowHeader(true);
+  };
+
+  const onSearchQueryChanged = async (searchQuery) => {
+    const photos = await fetchPhotosByQuery(searchQuery);
+    setPhotos(photos);
+    setShowHeader(false);
+  };
+
+  const getPhotoById = async (photoId) => {
+    const photo = await fetchPhotoById(photoId);
+    setModalData(photo);
+  };
+
+  const openModal = (photoId) => {
+    setModalData(null);
+    setModalOpen(true);
+    getPhotoById(photoId);
+  };
+
   return (
     <AppThemeProvider>
       <Paper>
@@ -37,6 +87,6 @@ const Home = () => {
       </Paper>
     </AppThemeProvider>
   );
-}
+};
 
-export default Home
+export default Home;
