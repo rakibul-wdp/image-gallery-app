2d36bd51dbfe01d0e8195202ac7f30199a371dba
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-19 04:25:20 +0600

    create photoGrid container component

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,18 @@
+import { Masonry } from "@mui/lab/Masonry";
+import { Container } from "@mui/material";
+import React from "react";
+import PhotoCard from "./PhotoCard";
+
+const PhotoGrid = ({ photos, onItemClick }) => {
+  return (
+    <Container fixed style={{ marginTop: 30, minHeight: "100vh" }}>
+      <Masonry columns={{ xs: 1, sm: 3 }} spacing={{ xs: 1, sm: 3 }}>
+        {photos.map((photo) => (
+          <PhotoCard key={photo.id} photo={photo} onItemClick={onItemClick} />
+        ))}
+      </Masonry>
+    </Container>
+  );
+};
+
+export default PhotoGrid;
