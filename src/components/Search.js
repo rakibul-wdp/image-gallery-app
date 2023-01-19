f79b4ec4349898131c873d1fdd7f7d597ccdf5a5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-19 06:25:20 +0600

    add style and also creae some functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  InputBase,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText
+} from "@mui/material";
+import { alpha, styled } from "@mui/system";
+import React, { useRef } from "react";
+import { useSearchQuery, useSearchQueryUpdate } from "./SearchQueryContext";
 
-const Search = () => {
+const Search = ({ dense = false }) => {
+  const searchBoxRef = useRef();
+  const updateSearchQuery = useSearchQueryUpdate();
+  const searchQuery = useSearchQuery();
+
+  const update = (e) => {
+    const query = e.target.value;
+    updateSearchQuery(query);
+  };
+
   return (
     <div style={{ position: "relative" }}>
       <SearchBar>
@@ -28,6 +47,56 @@ const Search = () => {
       </SuggestionList>
     </div>
   );
-}
+};
 
-export default Search
+// Styles
+const SearchBar = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius * 3,
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? theme.palette.grey[50]
+      : theme.palette.grey[200],
+  transition: "600ms all",
+  border: `1px solid ${alpha(theme.palette.common.black, 0.1)}`,
+  "& :hover": {},
+  width: "100%",
+  marginLeft: 0,
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  color: theme.palette.grey[500],
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: theme.palette.common.black,
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1.3, 1, 1.3, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+  },
+  "& .MuiInputBase-input::placeholder": {
+    color: theme.palette.grey[900],
+  },
+}));
+
+const SuggestionList = styled(List)(({ theme }) => ({
+  position: "absolute",
+  top: 60,
+  left: 0,
+  right: 0,
+  overflow: "hidden",
+  backgroundColor: theme.palette.common.white,
+  borderRadius: 5,
+  height: 0,
+  padding: 0,
+  color: theme.palette.common.black,
+}));
+
+export default Search;
