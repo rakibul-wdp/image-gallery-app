93f346a532fdfceeb9d18a562e3de800a45f8e0b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-25 10:23:34 +0600

    fix footer color in two mode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <FooterNav color="default">
+    <FooterNav>
       <Typography variant="body1" align="center">
         © {new Date().getFullYear()} Md. Rakibul Islam All rights reserved
       </Typography>
@@ -16,7 +16,14 @@ const FooterNav = styled("footer")(({theme}) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  margin: "40px 0 30px 0"
+  padding: "30px 0 30px 0",
+  color: theme.palette.mode === "dark" ? theme.palette.common.white : theme.palette.black,
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? theme.palette.common.black
+      : theme.palette.grey[50],
+  boxShadow:
+    theme.palette.mode === "dark" ? theme.shadows[0] : theme.shadows[4],
 }));
 
 export default Footer;
